50e111ae38fdd96bf3d763df5747fe065e27a3b1
GabenGar <87906913+GabenGar@users.noreply.github.com>
2023-08-25 15:25:52 +0500

    feat: local storage todos (#3)
    * local storage lib
    
    * local storage sync
    
    * styling and filing
    
    * fix: build errors

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,2 +1,3 @@
 export { Form } from "./form";
+export type { IFormElements } from "./form";
 export { Label } from "./label";
--- src/hooks/client.tsx
+++ src/hooks/client.tsx
@@ -1,8 +1,8 @@
-import { createContext } from "react";
+import { createContext, type ReactNode } from "react";
 
 const ClientContext = createContext({});
 
-export function ClientProvider({ children }) {
+export function ClientProvider({ children }: { children: ReactNode }) {
   return (
     <ClientContext.Provider value="null">{children}</ClientContext.Provider>
   );
