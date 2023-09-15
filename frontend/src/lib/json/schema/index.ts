1c170c2fa89710ec5a585693171a8f2560654c99
gabengar <gabengar@protonmail.com>
2023-09-15 15:19:48 +0500

    schema and config

--- frontend/src/lib/json/schema/index.ts
+++ frontend/src/lib/json/schema/index.ts
@@ -1,35 +1,38 @@
 import { addSchema, validate } from "@hyperjump/json-schema/draft-2020-12";
-// @ts-expect-error type def problem
 import { BASIC } from "@hyperjump/json-schema/experimental";
+import type { SchemaObject } from "@hyperjump/json-schema/schema/experimental";
 import dataExportSchema from "#schema/entities/data-export.schema.json";
 import taskSchema from "#schema/entities/task.schema.json";
+import taskStatusSchema from "#schema/entities/task-status.schema.json";
 import nanoidSchema from "#schema/strings/nanoid.schema.json";
 import dateTimeSchema from "#schema/dates/datetime.schema.json";
-import { logError } from "#lib/logs";
+import { logDebug, logError } from "#lib/logs";
 
-init();
+const metaSchema = "https://json-schema.org/draft/2020-12/schema";
 
-async function init() {
-  addSchema(
-    dataExportSchema,
-    toSchemaID(dataExportSchema.$id),
-    "https://json-schema.org/draft/2020-12/schema",
+init(metaSchema, [
+  dataExportSchema,
+  taskSchema,
+  taskStatusSchema,
+  nanoidSchema,
+  dateTimeSchema,
+]);
+
+async function init(
+  metaSchema: string,
+  schemas: (SchemaObject & { $id: string })[],
+) {
+  logDebug(
+    `Loading ${schemas.length} schemas with metaschema "${metaSchema}"...`,
   );
-  addSchema(
-    taskSchema,
-    toSchemaID(taskSchema.$id),
-    "https://json-schema.org/draft/2020-12/schema",
-  );
-  addSchema(
-    nanoidSchema,
-    toSchemaID(nanoidSchema.$id),
-    "https://json-schema.org/draft/2020-12/schema",
-  );
-  addSchema(
-    dateTimeSchema,
-    toSchemaID(dateTimeSchema.$id),
-    "https://json-schema.org/draft/2020-12/schema",
-  );
+
+  const addedIDs = schemas.map<ReturnType<typeof addSchema>>((schema) => {
+    const id = addSchema(schema, toSchemaID(schema.$id), metaSchema);
+
+    return id;
+  });
+
+  logDebug(`Loaded ${addedIDs.length} schemas with metaschema "${metaSchema}".`);
 }
 
 /**
