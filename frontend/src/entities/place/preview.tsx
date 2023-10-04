2e2824f38eb3be851fe659f16b953f05ebf4957f
gabengar <gabengar@protonmail.com>
2023-10-04 15:53:05 +0500

    creation form

--- frontend/src/entities/place/preview.tsx
+++ frontend/src/entities/place/preview.tsx
@@ -1,3 +1,4 @@
+import { ILocalization } from "#lib/localization";
 import { DescriptionList } from "#components";
 import { EntityID } from "#components/entities";
 import { Heading } from "#components/heading";
@@ -11,13 +12,14 @@ import {
 import type { IPlace } from "./types";
 
 interface IProps extends IPreviewProps {
+  translation: ILocalization["place"];
   place: IPlace;
 }
 
 export const PlacePreview = createBlockComponent(undefined, Component);
 
-function Component({ place, ...props }: IProps) {
-  const { id, title, description } = place;
+function Component({ translation, place, ...props }: IProps) {
+  const { id, title, description, created_at, updated_at } = place;
 
   return (
     <Preview {...props}>
@@ -29,7 +31,20 @@ function Component({ place, ...props }: IProps) {
           </PreviewHeader>
 
           <PreviewBody>
-            <DescriptionList></DescriptionList>
+            <DescriptionList
+              sections={[
+                [
+                  translation.description,
+                  description ?? translation.no_description,
+                ],
+              ]}
+            />
+            <DescriptionList
+              sections={[
+                [translation.created_at, created_at],
+                [translation.created_at, updated_at],
+              ]}
+            />
           </PreviewBody>
         </>
       )}
