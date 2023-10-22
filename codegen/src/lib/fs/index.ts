3b92784c529f02b3368e9bb62074ab6bc82dadd4
gabengar <gabengar@protonmail.com>
2023-10-22 13:38:01 +0500

    walk function

---
+++ codegen/src/lib/fs/index.ts
@@ -0,0 +1,1 @@
+export { walkFolder } from "./walk-folder.js";
---
+++ codegen/src/lib/fs/walk-folder.ts
@@ -0,0 +1,34 @@
+import path from "node:path";
+import { opendir } from "node:fs/promises";
+import type { Dirent, PathLike } from "node:fs";
+
+/**
+ * @TODO non-recursive walk
+ */
+export async function walkFolder(
+  folderPath: PathLike,
+  callback: (entry: Dirent, folderPath: string) => Promise<void>
+) {
+  console.debug(`Walking folder "${folderPath}"...`);
+
+  await walk(folderPath, callback);
+
+  console.debug(`Walked folder "${folderPath}".`);
+}
+
+async function walk(
+  folderPath: PathLike,
+  callback: (entry: Dirent, folderPath: string) => Promise<void>
+) {
+  const folder = await opendir(folderPath, { encoding: "utf8" });
+
+  for await (const entry of folder) {
+    if (!entry.isDirectory()) {
+      continue;
+    }
+
+    const nextFolderPath = path.join(folder.path, entry.name);
+    await callback(entry, nextFolderPath);
+    await walk(nextFolderPath, callback);
+  }
+}
