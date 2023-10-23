55d54b087ee4a40aafc1f2dcf1526805234f691a
GabenGar <87906913+GabenGar@users.noreply.github.com>
2023-10-23 12:42:54 +0500

    feat: linter and formatter for codegen (#36)
    * biome install and scripts
    
    * formatter config
    
    * more config stuff
    
    * extra rooo scripts

--- codegen/index.ts
+++ codegen/index.ts
@@ -1,6 +1,6 @@
+import { mkdtemp } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { mkdtemp } from "node:fs/promises";
 import { runCodegen } from "#codegen";
 
 const inputFolder = path.dirname(fileURLToPath(import.meta.url));
