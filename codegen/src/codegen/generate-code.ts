b3d6581a9a21b3b79075cf4947f5f9c1656f3eb2
gabengar <gabengar@protonmail.com>
2023-10-24 23:40:11 +0500

    codegen lib

--- codegen/src/codegen/generate-code.ts
+++ codegen/src/codegen/generate-code.ts
@@ -1,5 +1,14 @@
+import { mkdir, mkdtemp, rename, rm, writeFile } from "node:fs/promises";
+import { tmpdir } from "node:os";
 import path from "node:path";
 import type { IGeneratorMap, IModuleInfo } from "./types.js";
+
+const codeMessage = [
+  "/**",
+  " * This module was generated automatically.",
+  " * Do not edit it manually.",
+  " */",
+].join("\n");
 
 export async function generateCode(
   outputFolder: string,
@@ -7,18 +16,35 @@ export async function generateCode(
 ) {
   console.debug(`Generating code for ${generators.size} generators...`);
 
+  console.debug("Creating a temporary folder...");
+  const temporaryPath = await mkdtemp(path.join(tmpdir()));
+  console.debug(`Created the temporary folder "${temporaryPath}".`);
+
   for await (const [_, codeGenerator] of generators) {
     console.debug(`Generating code for generator "${codeGenerator.name}"...`);
 
     const { name: generatorName, generate } = codeGenerator;
-    const generatorPath = path.join(outputFolder, generatorName);
+    const generatorPath = path.join(temporaryPath, generatorName);
     const modulesInfo = await generate();
+    await mkdir(generatorPath, { recursive: true });
 
     for await (const moduleInfo of modulesInfo) {
+      await saveModule(generatorPath, moduleInfo);
     }
 
     console.debug(`Generated code for generator "${codeGenerator.name}".`);
   }
+
+  console.debug(
+    `Replacing the contents of folder "${outputFolder}" with folder "${temporaryPath}"...`,
+  );
+  const backupTempPath = await mkdtemp(path.join(tmpdir()));
+  const backupPath = path.join(backupTempPath, "backup");
+  await rename(outputFolder, backupPath);
+  await rename(temporaryPath, outputFolder)
+  await rm(backupTempPath)
+
+  console.debug(`Replaced the contents of folder "${outputFolder}".`);
 
   console.debug(`Generated code for ${generators.size} generators.`);
 }
@@ -30,6 +56,11 @@ async function saveModule(
   console.debug(`Generating code for the module "${name}"...`);
 
   const modulePath = path.join(generatorPath, `${name}.ts`);
+  const fileContent = [codeMessage, content].join("\n");
+
+  // generator folder is created ahead of time
+  // so there is no need for ensuring folder existence
+  await writeFile(modulePath, fileContent, { encoding: "utf8" });
 
   console.debug(`Generated code for the module "${name}".`);
 }
