26a55a1c0a025a6de307596c2028e3a95951499a
gabengar <gabengar@protonmail.com>
2023-11-11 01:59:47 +0500

    chore: schema collector

--- frontend/src/codegen/generators/json-schema/generator.ts
+++ frontend/src/codegen/generators/json-schema/generator.ts
@@ -1,4 +1,17 @@
-import type { IGeneratedNestedModule, IModuleInfo } from "../../../../../codegen/src/codegen/types.js";
+import path from "node:path";
+import type {
+  IGeneratedNestedModule,
+  IModuleInfo,
+} from "../../../../../codegen/src/codegen/types.js";
+// @TODO import from a core lib once it's implemented
+import { reduceFolder } from "../../../../../codegen/src/lib/fs/index.js";
+
+const schemaFolderPath = path.resolve("../../../../../schema");
+/**
+ * The key is the path to the schema file relative to
+ */
+interface ISchemaMap extends Map<string, { $id: string }> {}
+
 
 async function generateJSONSchemas(): Promise<IGeneratedNestedModule> {
   const content = `export const ROCKET_SHIP = ""`;
@@ -10,6 +23,12 @@ async function generateJSONSchemas(): Promise<IGeneratedNestedModule> {
   const modulesData = [moduleData];
 
   return modulesData;
+}
+
+async function getSchemas(): Promise<ISchemaMap> {
+  const schemaMap = new Map();
+
+  return schemaMap;
 }
 
 export default generateJSONSchemas;
