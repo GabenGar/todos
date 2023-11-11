c0a7f83f0728a8529616a18827336e4fae32e663
gabengar <gabengar@protonmail.com>
2023-11-12 01:09:57 +0500

    feat: index files codegen

--- codegen/src/codegen/generate-nested-code.ts
+++ codegen/src/codegen/generate-nested-code.ts
@@ -2,8 +2,9 @@ import { mkdir, writeFile } from "node:fs/promises";
 import path from "node:path";
 import {
   type IGeneratedNestedModule,
-  IModuleInfo,
+  type IModuleInfo,
   codeMessage,
+  indexFileName,
 } from "./types.js";
 
 export async function generateNestedCode(
@@ -19,6 +20,7 @@ export async function generateNestedCode(
 
   for await (const [moduleName, moduleInfos] of moduleMap) {
     const moduleFolderPath = path.join(generatorPath, moduleName);
+    const indexModules = new Map<string, IModuleInfo["exports"]>();
 
     await mkdir(moduleFolderPath, { recursive: true });
 
@@ -28,21 +30,62 @@ export async function generateNestedCode(
         .padStart(moduleMap.size.toString().length, "0");
 
       await generateModule(moduleInfo, moduleFolderPath);
+      indexModules.set(moduleInfo.name, moduleInfo.exports);
 
       console.debug(
         `Generated code for the module "${moduleInfo.name}" (${formattedModuleCount} out of ${moduleMap.size}).`,
       );
       moduleCount++;
     }
+
+    await saveIndexFile(moduleFolderPath, indexModules);
   }
 
   console.debug(`Generated nested module "${name}" at "${generatorPath}".`);
 }
 
 async function generateModule(moduleInfo: IModuleInfo, folderPath: string) {
-  const { name, content, exports } = moduleInfo;
+  const { name, content } = moduleInfo;
   const moduleFilePath = path.join(folderPath, `${name}.ts`);
   const fileContent = [codeMessage, content].join("\n");
 
   await writeFile(moduleFilePath, fileContent, { encoding: "utf8" });
+}
+
+async function saveIndexFile(
+  modulePath: string,
+  indexModules: Map<string, IModuleInfo["exports"]>,
+) {
+  console.debug(`Generating index file for module "${modulePath}"...`);
+
+  const indexFilePath = path.join(modulePath, indexFileName);
+  const indexContent = Array.from(indexModules)
+    .reduce<string[]>((lines, [moduleName, moduleExports]) => {
+      if (!moduleExports.length) {
+        throw new Error(
+          `Generated code must export at least one symbol and the module "${moduleName}" exported none.`,
+        );
+      }
+
+      const symbolExports = moduleExports
+        .map<string>(({ name, type, alias }) => {
+          const symbolString = `${type === "abstract" ? `type ${name}` : name}${
+            alias ? ` as ${alias}` : ""
+          }`;
+
+          return symbolString;
+        })
+        .join(", ");
+
+      const exportStatement = `export {${symbolExports}} from "./${moduleName}.js"`;
+
+      lines.push(exportStatement);
+
+      return lines;
+    }, [])
+    .join("\n");
+
+  await writeFile(indexFilePath, indexContent, { encoding: "utf8" });
+
+  console.debug(`Generated index file for module "${modulePath}".`);
 }
