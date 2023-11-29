f283d8dfad42d74c529c16ac43c3b011f1e16b29
GabenGar <87906913+GabenGar@users.noreply.github.com>
2023-11-29 23:04:36 +0500

    feat: move data export/import forms to account page (#52)
    * feat: move import/export forms to account page
    
    * format and minor version up

--- frontend/src/app/[lang]/account/client.tsx
+++ frontend/src/app/[lang]/account/client.tsx
@@ -1,56 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
-import { ILocalizationPage } from "#lib/localization";
+import { useRouter } from "next/navigation";
+import type { ILocalizationPage } from "#lib/localization";
 import { useClient } from "#hooks";
 import type { ITranslatableProps } from "#components/types";
-import { Details, DetailsBody, DetailsHeader } from "#components/details";
+import {
+  Details,
+  DetailsBody,
+  DetailsFooter,
+  DetailsHeader,
+} from "#components/details";
 import { DescriptionList, DescriptionSection, Loading } from "#components";
 import { Heading } from "#components/heading";
 import { Link } from "#components/link";
 import { Pre } from "#components/pre";
+import { DataExportForm, ImportDataExportForm } from "#entities/data-export";
 
 interface IProps extends ITranslatableProps {
   translation: ILocalizationPage["account"];
 }
 
 export function Client({ commonTranslation, translation }: IProps) {
-  const clientInfo = useClient();
-
-  if (!clientInfo.isClient) {
-    return (
-      <Details headingLevel={2}>
-        {(headingLevel) => (
-          <>
-            <DetailsHeader>
-              <Heading
-                // @ts-expect-error type widening
-                level={headingLevel + 1}
-              >
-                {translation["Compatibility"]}
-              </Heading>
-            </DetailsHeader>
-
-            <DetailsBody>
-              <DescriptionList>
-                <DescriptionSection
-                  isHorizontal
-                  dKey={
-                    <Link href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">
-                      <Pre>localeStorage</Pre>
-                    </Link>
-                  }
-                  dValue={<Loading />}
-                />
-              </DescriptionList>
-            </DetailsBody>
-          </>
-        )}
-      </Details>
-    );
-  }
-
-  const { compatibility } = clientInfo;
+  const router = useRouter();
 
   return (
     <Details headingLevel={2}>
@@ -61,29 +32,72 @@ export function Client({ commonTranslation, translation }: IProps) {
               // @ts-expect-error type widening
               level={headingLevel + 1}
             >
-              {translation["Compatibility"]}
+              {translation["Data"]}
             </Heading>
           </DetailsHeader>
 
           <DetailsBody>
-            <DescriptionList>
-              <DescriptionSection
-                isHorizontal
-                dKey={
-                  <Link href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">
-                    <Pre>localeStorage</Pre>
-                  </Link>
-                }
-                dValue={
-                  compatibility
-                    ? translation["Supported"]
-                    : translation["Not supported"]
-                }
-              />
-            </DescriptionList>
+            <Heading
+              // @ts-expect-error type widening
+              level={headingLevel + 2}
+            >
+              {translation["Export"]}
+            </Heading>
+            <DataExportForm translation={translation} />
+
+            <Heading
+              // @ts-expect-error type widening
+              level={headingLevel + 2}
+            >
+              {translation["Import"]}
+            </Heading>
+            <ImportDataExportForm
+              commonTranslation={commonTranslation}
+              translation={translation}
+              id="import-data-export"
+              onSuccess={async () => router.refresh()}
+            />
           </DetailsBody>
+
+          <DetailsFooter>
+            <Heading
+              // @ts-expect-error type widening
+              level={headingLevel + 1}
+            >
+              {translation["Compatibility"]}
+            </Heading>
+            <Compatibility translation={translation} />
+          </DetailsFooter>
         </>
       )}
     </Details>
+  );
+}
+
+interface ICompatibilityProps extends Pick<IProps, "translation"> {}
+
+function Compatibility({ translation }: ICompatibilityProps) {
+  const clientInfo = useClient();
+
+  return (
+    <DescriptionList>
+      <DescriptionSection
+        isHorizontal
+        dKey={
+          <Link href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">
+            <Pre>localeStorage</Pre>
+          </Link>
+        }
+        dValue={
+          !clientInfo.isClient ? (
+            <Loading />
+          ) : clientInfo.compatibility ? (
+            translation["Supported"]
+          ) : (
+            translation["Not supported"]
+          )
+        }
+      />
+    </DescriptionList>
   );
 }
