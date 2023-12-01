5587d6a65480dd8a713fc9a7260eb1dcce1f7494
gabengar <gabengar@protonmail.com>
2023-12-01 12:11:03 +0500

    style: logger switcher

--- frontend/src/components/logger-switcher.tsx
+++ frontend/src/components/logger-switcher.tsx
@@ -2,6 +2,9 @@
 import type { ILocalization } from "#lib/localization";
 import { useClient } from "#hooks";
 import { Loading } from "./loading";
+import { DescriptionList, DescriptionSection } from "./description-list";
+
+import styles from "./logger-switcher.module.scss";
 
 interface IProps {
   translation: ILocalization["layout"];
@@ -9,12 +12,34 @@ interface IProps {
 
 export function LoggerSwitcher({ translation }: IProps) {
   const clientInfo = useClient();
+  const loggerTranslation = translation.logger;
+  const logLevelTranslation = {
+    debug: "Debug",
+    log: "Logs",
+    info: "Information",
+    warn: "Warnings",
+    error: "Errors",
+  } as const;
 
-  return !clientInfo.isClient ? (
-    <Loading />
-  ) : (
-    <>
-      {translation.log_level}: {clientInfo.logLevel}
-    </>
+  return (
+    <DescriptionList>
+      <DescriptionSection
+        isHorizontal
+        dKey={loggerTranslation["Log level"]}
+        dValue={
+          !clientInfo.isClient ? (
+            <Loading />
+          ) : (
+            <span className={styles[clientInfo.logLevel]}>
+              {
+                loggerTranslation.levels[
+                  logLevelTranslation[clientInfo.logLevel]
+                ]
+              }
+            </span>
+          )
+        }
+      />
+    </DescriptionList>
   );
 }
