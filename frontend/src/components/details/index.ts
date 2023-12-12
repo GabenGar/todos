8e90d8d75f84e71649f8f36944b362c8ab684411
gabengar <gabengar@protonmail.com>
2023-12-12 19:34:22 +0500

    feat: list component

--- frontend/src/components/details/index.ts
+++ frontend/src/components/details/index.ts
@@ -1,3 +1,6 @@
+/**
+ * @TODO rename to `Overview` sicne `Details` is a different unrelated component
+ */
 export { Details, DetailsHeader, DetailsBody, DetailsFooter } from "./details";
 export { DetailsPlaceHolder } from "./placeholder";
 export type {
