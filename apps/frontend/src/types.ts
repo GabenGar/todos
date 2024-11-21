bdd07f5b489fee79b6b529ddcb9c9f5abf396d1e
GabenGar <87906913+GabenGar@users.noreply.github.com>
2024-11-21 23:02:09 +0500

    feat: frontend github pages (#85)
    * feat(frontend): static export
    
    * feat: github pages ci (probably)

--- apps/frontend/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type IOneOrMany<Type> = Type | readonly Type[];
-
-export type IPartialSome<Type, Key extends keyof Type> = Omit<Type, Key> &
-  Pick<Partial<Type>, Key>;
-
-export type IRequiredSome<Type, Key extends keyof Type> = Omit<Type, Key> &
-  Pick<Required<Type>, Key>;
