9c2242f71f96f07980a2ee91f1e5fcb1eb1dfdb4
gabengar <gabengar@protonmail.com>
2024-11-22 16:51:21 +0500

    feat: support subpath pages build

---
+++ apps/frontend/next.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import nextEnv from "@next/env";
+
+const projectDir = process.cwd();
+nextEnv.loadEnvConfig(projectDir);
+
+/**
+ * @returns {Promise<import('next').NextConfig>}
+ */
+async function createNextConfig() {
+  const baseURL = process.env.NEXT_PUBLIC_SITE_BASE_URL;
+  const basePathname = !baseURL
+    ? undefined
+    : new URL(baseURL, "https://example.com").pathname;
+  const basePath =
+    !basePathname || basePathname === "/" ? undefined : basePathname;
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    basePath,
+    output: "export",
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    distDir: "dist",
+    experimental: {
+      typedRoutes: true,
+    },
+  };
+
+  return nextConfig;
+}
+
+export default createNextConfig;
---
+++ apps/frontend/src/lib/urls/internal-url.ts
@@ -0,0 +1,1 @@
+export class InternalURL extends URL {}
