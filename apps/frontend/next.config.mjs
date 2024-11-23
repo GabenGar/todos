0d0beae0ee0e7f828a3daac057c8f785902e702c
gabengar <gabengar@protonmail.com>
2024-11-23 12:29:20 +0500

    chore: frontend deps

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -23,6 +23,13 @@ async function createNextConfig() {
     trailingSlash: true,
     skipTrailingSlashRedirect: true,
     distDir: "dist",
+    sassOptions: {
+      // a duct tape
+      // https://github.com/vercel/next.js/discussions/67931#discussioncomment-11044560
+      // until nextjs releases a next major
+      // https://github.com/vercel/next.js/issues/71638#issuecomment-2454463904
+      silenceDeprecations: ["legacy-js-api"],
+    },
     experimental: {
       typedRoutes: true,
     },
