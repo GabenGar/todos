65f6848fddb658bf2bd0188f777d8543ae9d7608
gabengar <gabengar@protonmail.com>
2025-03-16 00:50:34 +0500

    chore: logout

--- apps/oikia/src/routes/authentication/logout.tsx
+++ apps/oikia/src/routes/authentication/logout.tsx
@@ -1,23 +1,34 @@
-import { Page } from "@repo/ui/pages";
+import { data } from "react-router";
+import { createServerAction } from "#server/lib/router";
+import { destroySession, getSession } from "#server/lib/sessions";
+import { createSuccessfullAPIResponse } from "#server/lib/api";
+
 import type { Route } from "./+types/logout";
-import { Overview } from "@repo/ui/articles";
 
-export function meta({ error }: Route.MetaArgs) {
-  return [{ title: "Logout" }];
+export function headers({ actionHeaders, loaderHeaders }: Route.HeadersArgs) {
+  return loaderHeaders ? loaderHeaders : actionHeaders;
 }
 
-function LogoutPage() {
-  const heading = "Logout";
+export const action = createServerAction(
+  async ({ request }: Route.ActionArgs) => {
+    switch (request.method) {
+      case "POST": {
+        const session = await getSession(request.headers.get("Cookie"));
 
-  return (
-    <Page heading={heading}>
-      <Overview headingLevel={2}>{}</Overview>
-    </Page>
-  );
-}
+        const headers = new Headers([
+          ["Set-Cookie", await destroySession(session)],
+        ]);
 
-export async function action() {
-  throw new Error("Not Implemented.");
-}
+        const response = data(createSuccessfullAPIResponse(true), {
+          headers,
+        });
 
-export default LogoutPage;
+        return response;
+      }
+
+      default: {
+        throw new Error(`Unknown method "${request.method}".`);
+      }
+    }
+  }
+);
