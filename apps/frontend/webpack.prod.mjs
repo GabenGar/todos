a3a14e827783c5b715cba2a9d6b2450bd73b60e2
gabengar <gabengar@protonmail.com>
2025-10-20 23:13:54 +0500

    feat: async prod config

--- apps/frontend/webpack.prod.mjs
+++ apps/frontend/webpack.prod.mjs
@@ -5,34 +5,38 @@ import { fileURLToPath } from "node:url";
 import { merge } from "webpack-merge";
 import commonConfiguration from "./webpack.common.mjs";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const outputPath = path.resolve(__dirname, "out");
+async function createProdConfig() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const outputPath = path.resolve(__dirname, "out");
 
-// doing dev worker output cleanup here
-// because setting up an adhoc script is a gigantic pain
-const devOutputPath = path.join(__dirname, "public");
-const devWorkerPath = path.resolve(devOutputPath, "service-worker.js");
-const devWorkerMapPath = path.resolve(devOutputPath, "service-worker.js.map");
+  // doing dev worker output cleanup here
+  // because setting up an adhoc script is a gigantic pain
+  const devOutputPath = path.join(__dirname, "public");
+  const devWorkerPath = path.resolve(devOutputPath, "service-worker.js");
+  const devWorkerMapPath = path.resolve(devOutputPath, "service-worker.js.map");
 
-await fs.rm(devWorkerPath, { force: true });
-await fs.rm(devWorkerMapPath, { force: true });
+  await fs.rm(devWorkerPath, { force: true });
+  await fs.rm(devWorkerMapPath, { force: true });
 
-/**
- * @type {import("webpack").Configuration}
- */
-const prodConfig = {
-  name: "prod-config",
-  mode: "production",
-  devtool: "source-map",
-  optimization: {
-    runtimeChunk: false,
-  },
-  output: {
-    path: outputPath,
-    filename: "[name].js",
-  },
-};
+  /**
+   * @type {import("webpack").Configuration}
+   */
+  const prodConfig = {
+    name: "prod-config",
+    mode: "production",
+    devtool: "source-map",
+    optimization: {
+      runtimeChunk: false,
+    },
+    output: {
+      path: outputPath,
+      filename: "[name].js",
+    },
+  };
 
-const finalConfig = merge(commonConfiguration, prodConfig);
+  const finalConfig = merge(commonConfiguration, prodConfig);
 
-export default finalConfig;
+  return finalConfig;
+}
+
+export default createProdConfig;
