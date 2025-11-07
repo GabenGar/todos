9a31676a67aa4d2d8b14ff2a7fd81f0a880b7870
gabengar <gabengar@protonmail.com>
2025-11-07 23:33:40 +0500

    chore: log build env var

--- apps/frontend/webpack.prod.mjs
+++ apps/frontend/webpack.prod.mjs
@@ -25,6 +25,8 @@ async function createProdConfig() {
   await fs.rm(workerPath, { force: true });
   await fs.rm(workerMapPath, { force: true });
 
+  console.log(process.env.NEXT_PUBLIC_IS_SERVICE_WORKER_ENABLED)
+
   const staticPaths = await collectStaticPaths(outputPath);
   const assetsHash = await getAssetsHash(staticPaths);
   /**
