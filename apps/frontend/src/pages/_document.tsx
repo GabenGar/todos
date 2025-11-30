c77dd6e2e52f6c2a2c128da1d38150ceec8ff362
gabengar <gabengar@protonmail.com>
2025-11-30 22:16:29 +0500

    wip: pwa manifest

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { DEFAULT_LOCALE } from "#lib/internationalization";
+import { SITE_BASE_PATHNAME } from "#environment";
+
+const manifestURL = `${SITE_BASE_PATHNAME}/manifest.json`;
 
 function Document() {
   return (
     <Html lang={DEFAULT_LOCALE} prefix="og: https://ogp.me/ns#">
-      <Head />
+      <Head>
+        <link rel="manifest" href={manifestURL} />
+      </Head>
       <body>
         <Main />
         <NextScript />
