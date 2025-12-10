1a5a2b943da38dc5068e4fc3c9fc47d5c16f3a30
gabengar <gabengar@protonmail.com>
2025-12-10 22:10:31 +0500

    wip: pwa plugin

--- packages/webpack/src/pwa-webpack-plugin/options.ts
+++ packages/webpack/src/pwa-webpack-plugin/options.ts
@@ -1,4 +1,11 @@
 export interface IPWAWebpackPluginOptions {
   name: string;
   short_name?: string;
+  id?: string;
+  description?: string;
+  icon?: string;
+  start_url?: string;
+  display?: "standalone" | "browser" | "fullscreen" | "minimal-ui";
+  theme_color?: string;
+  background_color: string;
 }
